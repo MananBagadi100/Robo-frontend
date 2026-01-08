351079ee3bafc8f177c759997e8876b443fda6cb
MananBagadi100 <mananbagadi100@gmail.com>
2026-01-08 17:20:35 +0530

    Migrated whole of app.jsx functionality into Home.jsx because of creation of multiple routes and to make it a single page application . Also added admin dashboard button for easy navigation

---
+++ src/Admin.jsx
@@ -0,0 +1,9 @@
+import './Admin.css'
+const Admin = () => {
+    return (
+        <>
+            <div>Admin Page</div>
+        </>
+    )
+}
+export default Admin
--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import { useState } from "react";
 import "./App.css";
 import { generatePost , fetchGenerationStatus} from "./utils/api";
 import * as motion from "motion/react-client"
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import Admin from "./Admin";
+import Home from "./Home";
+
 
 function App() {
-    const [prompt, setPrompt] = useState("");
-    const [loading, setLoading] = useState(false);
-    const [promptLengthError , setPromptLengthError] = useState(false)
-    const [result, setResult] = useState(null); //to store the result response (if api successful)
-    const [serverError , setServerError] = useState('') //state to store server error (if any)
+    // const [prompt, setPrompt] = useState("");
+    // const [loading, setLoading] = useState(false);
+    // const [promptLengthError , setPromptLengthError] = useState(false)
+    // const [result, setResult] = useState(null); //to store the result response (if api successful)
+    // const [serverError , setServerError] = useState('') //state to store server error (if any)
 
     const handleGenerate = async () => {
         setServerError('')      //resetting the server error after every click 
@@ -86,92 +90,104 @@ function App() {
         link.download = "post.png";
         link.click();
     };
-    console.log('(frontend is printing this!) the server error is ',serverError)
+    // console.log('(frontend is printing this!) the server error is ',serverError)
 
     return (
-        <div className="app-container">
-            <motion.h1 
-                initial={{opacity:0,scale:0.8}}
-                animate={{opacity:1,scale:1}}
-                transition={{duration:1}}
-                className="app-title"
-            >
-                AI Social Media Post Creator
-            </motion.h1>
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/admin" element={<Admin />} />
+                </Routes>
+                {/* <section className="app-top-section-area">
+                    <Link to='/admin' className="app-top-section-admin-btn">Admin Dashboard</Link>
+                </section>
+                
+                <div className="app-container">
+                    <motion.h1 
+                        initial={{opacity:0,scale:0.8}}
+                        animate={{opacity:1,scale:1}}
+                        transition={{duration:1}}
+                        className="app-title"
+                    >
+                        AI Social Media Post Creator
+                    </motion.h1>                        
 
-            <motion.div 
-                initial={{opacity:0,y:30}}
-                animate={{opacity:1,y:0}}
-                transition={{duration:0.8,delay:0.2}}
-                viewport={{once:true}}
-                className="card"
-            >
-                <div className="subheading-text-wrapper">
-                  <span className="subheading-text">Create your next social media post with just One Click !</span>
-                </div>
-                <textarea
-                    className="prompt-input"
-                    placeholder="Describe your idea..."
-                    value={prompt}
-                    onChange={(e) => setPrompt(e.target.value)}
-                />
+                    <motion.div 
+                        initial={{opacity:0,y:30}}
+                        animate={{opacity:1,y:0}}
+                        transition={{duration:0.8,delay:0.2}}
+                        viewport={{once:true}}
+                        className="card"
+                    >
+                        <div className="subheading-text-wrapper">
+                        <span className="subheading-text">Create your next social media post with just One Click !</span>
+                        </div>
+                        <textarea
+                            className="prompt-input"
+                            placeholder="Describe your idea..."
+                            value={prompt}
+                            onChange={(e) => setPrompt(e.target.value)}
+                        />
 
-                {promptLengthError ? 
-                    <div className="prompt-error-msg">Prompt must have minimum 20 charecters</div> 
-                    : '' }
+                        {promptLengthError ? 
+                            <div className="prompt-error-msg">Prompt must have minimum 20 charecters</div> 
+                            : '' }
 
-                {serverError && (
-                    <div className="server-error-msg">{serverError}</div>
-                )}
+                        {serverError && (
+                            <div className="server-error-msg">{serverError}</div>
+                        )}
 
-                <div className="generate-btn-wrapper">
-                  <motion.button
-                      whileHover={{scale:1.1}}
-                      whileTap={{scale:0.8}}
-                      transition={{duration:0.1,ease:"easeOut"}}
-                      className="generate-btn"
-                      onClick={handleGenerate}
-                      disabled={loading}
-                  >
-                      {loading ? "Generating..." : "Generate"}
-                  </motion.button>
-                </div>
-            </motion.div>
+                        <div className="generate-btn-wrapper">
+                        <motion.button
+                            whileHover={{scale:1.1}}
+                            whileTap={{scale:0.8}}
+                            transition={{duration:0.1,ease:"easeOut"}}
+                            className="generate-btn"
+                            onClick={handleGenerate}
+                            disabled={loading}
+                        >
+                            {loading ? "Generating..." : "Generate"}
+                        </motion.button>
+                        </div>
+                    </motion.div>
 
-            { /* Only rendered when backend gives response */}
-            {result && (       
-                <div className="result-card">
-                    <h2 className="caption">{result.caption}</h2>
+                    
+                    {result && (       
+                        <div className="result-card">
+                            <h2 className="caption">{result.caption}</h2>
 
-                    <div className="hashtags">
-                        {result.hashtags.map((tag) => (
-                            <span key={tag} className="tag">
-                                {tag}
-                            </span>
-                        ))}
-                    </div>
+                            <div className="hashtags">
+                                {result.hashtags.map((tag) => (
+                                    <span key={tag} className="tag">
+                                        {tag}
+                                    </span>
+                                ))}
+                            </div>
 
-                    <img
-                        className="result-image"
-                        src={`data:image/png;base64,${result.imageBase64}`}
-                        alt="Generated"
-                    />
-                    <div className="actions">
-                        <button className="action-btn" onClick={handleCopyCaption}>
-                            Copy Caption
-                        </button>
+                            <img
+                                className="result-image"
+                                src={`data:image/png;base64,${result.imageBase64}`}
+                                alt="Generated"
+                            />
+                            <div className="actions">
+                                <button className="action-btn" onClick={handleCopyCaption}>
+                                    Copy Caption
+                                </button>
 
-                        <button className="action-btn" onClick={handleCopyHashtags}>
-                            Copy Hashtags
-                        </button>
+                                <button className="action-btn" onClick={handleCopyHashtags}>
+                                    Copy Hashtags
+                                </button>
 
-                        <button className="action-btn" onClick={handleDownload}>
-                            Download Image
-                        </button>
-                    </div>
-                </div>
-            )}
-        </div>
+                                <button className="action-btn" onClick={handleDownload}>
+                                    Download Image
+                                </button>
+                            </div>
+                        </div>
+                    )}
+                </div> */}
+            </BrowserRouter>
+        </>
     );
 }
 
