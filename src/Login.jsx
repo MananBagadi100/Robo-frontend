0e71ea6d6bb4bdf8187bc54e9d16e7ab18574fe1
MananBagadi100 <mananbagadi100@gmail.com>
2026-05-16 12:55:54 +0530

    added login css for deployment

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,33 +1,56 @@
 import { Link } from 'react-router-dom'
 import './Login.css'
+
 const Login = () => {
     return (
-        <>  
+        <>
             <main className="login-full-page-container">
                 <section className="login-dialog-box">
                     <header className="login-dialog-box-header">
-                        <h1 className="login-dialog-box-heading">Login</h1>
+                        <h1 className="login-dialog-box-heading">
+                            Welcome Back
+                        </h1>
+
+                        <p className="login-subtitle">
+                            Login to continue using Robo AI
+                        </p>
                     </header>
 
                     <div className="login-dialog-box-info-area">
                         <form className="login-dialog-box-form-area">
                             <div className="login-form-fields">
                                 <label>Email</label>
-                                <input type="text" defaultValue="email@example.com" autoComplete='email'/>
+
+                                <input
+                                    type="text"
+                                    defaultValue="email@example.com"
+                                    autoComplete="email"
+                                />
                             </div>
 
                             <div className="login-form-fields">
                                 <label>Password</label>
-                                <input type="password" defaultValue="password" autoComplete='current-password'/>
+
+                                <input
+                                    type="password"
+                                    defaultValue="password"
+                                    autoComplete="current-password"
+                                />
                             </div>
 
                             <div className="login-submit-btn-wrapper">
-                                <Link className="login-submit-btn" to='/admin'>
+                                <Link
+                                    className="login-submit-btn"
+                                    to="/admin"
+                                >
                                     Login
                                 </Link>
                             </div>
 
-                            <p>Demo credentials already added for smooth checkin !</p>
+                            <p className="login-demo-text">
+                                Demo credentials already added for smooth
+                                checking!
+                            </p>
                         </form>
                     </div>
                 </section>
@@ -35,4 +58,5 @@ const Login = () => {
         </>
     )
 }
+
 export default Login
